9f9d3663be10ee42f162995e5259fb13e5f105fc
daniel-12 <51939760+daniel-12@users.noreply.github.com>
2019-08-10 14:20:32 -0700

    Added entity Database

---
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class DataContext : DbContext  // db context is a class that exists in the entity framework.
+    {
+        public DataContext() : base("DefaultConnection") // We're passing our connection string.
+        {
+
+        }
+        // DbSets help specify which classes you do want to store in the data base.
+        public DbSet<Product> Products { get; set; }    
+        public DbSet<ProductCategory> ProductsCategories { get; set; }
+    }
+}
